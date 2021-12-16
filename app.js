c1cac8b985e990866ff496552b7540ab7e1e958b
gabisanto <gabrielasantoriello@gmail.com>
2021-12-16 19:18:41 -0300

    cambios en app.js y package.json para heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require ("path")
 const express = require ("express")
 const app = express ()
 
-app.set ("port", 3001)
+app.set ("port", process.env.PORT || 3001)
 
 app.listen (app.get("port"), () => {
     console.log ("Servidor OK");
