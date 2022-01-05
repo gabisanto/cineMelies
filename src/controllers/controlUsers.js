98eb12d9aa670b5459977d49a65128b87ee47a29
florCar <florbelcarabaja@gmail.com>
2022-01-05 14:42:12 -0300

    cambios css de home y se agrego  controlador de register/login con sus include

--- src/controllers/controlUsers.js
+++ src/controllers/controlUsers.js
@@ -1,4 +1,10 @@
 module.exports = {
-    login: (req,res) => res.render('./users/login'),
-    register: (req,res) => res.render('./users/register')
+    login: (req,res) => res.render('./users/login',{
+        styles: ['login'],
+        title: 'Inicio de sesión'
+    }),
+    register: (req,res) => res.render('./users/register',{
+        styles: ['register'],
+        title: 'Registrarse'
+    })
 }
