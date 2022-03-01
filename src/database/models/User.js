7f5dca26c67aef6e4555d30002b8b837296844bc
gabriel030 <gabriel.r.antonietti@gmail.com>
2022-03-01 12:06:04 -0300

    terminada la tabla de User

---
+++ src/database/models/User.js
@@ -0,0 +1,59 @@
+module.exports = (sequelize, dataTypes) => { 
+    let alias = 'User'; 
+    let cols = { 
+        id: { 
+            type: dataTypes.BIGINT(10).UNSIGNED, 
+            primaryKey: true, 
+            allowNull: false, 
+            autoIncrement: true
+        }, 
+        name: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        }, 
+        document: {
+            type: dataTypes.BIGINT(11), 
+            allowNull: false
+        },
+        birthDate: {
+            type: dataTypes.DATEONLY, 
+            allowNull: false
+        },
+        email: {
+            type: dataTypes.STRING(100), 
+            allowNull: false
+        }, 
+        password: {
+            type: dataTypes.STRING(100), 
+            allowNull: false
+        },
+        admin : { 
+            type: dataTypes.BIGINT(10), 
+            allowNull: false
+        },
+        isActive: { 
+            type: dataTypes.BIGINT(10), 
+            allowNull: false
+        }, 
+        avatar_id: {
+            type: dataTypes.BIGINT(10), 
+            allowNull: false
+        }
+
+
+    }; 
+    let config = {
+        timestamp: true, 
+        tableName: 'Users'
+    };
+
+    const User = sequelize.define(alias, cols, config); 
+
+    //Creacion de relaciones 
+
+
+    
+
+
+
+}
